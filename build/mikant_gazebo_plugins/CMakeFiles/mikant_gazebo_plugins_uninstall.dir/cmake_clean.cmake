7b8f84ce47ceb27c4090a29629b0296362910c3e
thurn6101 <thurn6101@gmail.com>
2022-12-10 04:29:57 +0700

    stupid commit

--- build/mikant_gazebo_plugins/CMakeFiles/mikant_gazebo_plugins_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mikant_gazebo_plugins_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mikant_gazebo_plugins_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/mikant_sim/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/thirawat/Mikant/mikant_sim_ws/install/mikant_sim'
