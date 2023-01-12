9a5f77ae5197dbe1e444886f475d66921b516219
thurn6101 <thurn6101@gmail.com>
2023-01-12 23:47:18 +0700

    apparent draft 1

--- src/mikant_gazebo_plugins/src/lift_drag_force_plugin.cc
+++ src/mikant_gazebo_plugins/src/lift_drag_force_plugin.cc
@@ -5,6 +5,8 @@
 #include <gazebo/common/Events.hh>
 #include <ignition/math/Vector3.hh>
 #include <gazebo_ros/node.hpp>
+#include <math.h>
+#include <ignition/math/Matrix3.hh>
 
 #include <rclcpp/rclcpp.hpp>
 #include "std_msgs/msg/float64_multi_array.hpp"
@@ -12,7 +14,7 @@
 #include <string>
 
 namespace lift_drag_force_plugin
-
+{
   LiftDragForcePlugin::LiftDragForcePlugin()
     : fluid_density(1.225),
       center_of_pressure(0, 0, 0)
@@ -53,6 +55,10 @@ namespace lift_drag_force_plugin
     {
       this->cda = _sdf->Get<double>("cda");
     }
+    if(_sdf->HasElement("center_of_pressure"))
+    {
+      this->center_of_pressure = _sdf->Get<double>("center_of_pressure");
+    }
     if(_sdf->HasElement("init_angle_of_attack"))
     {
       this->init_angle_of_attack = _sdf->Get<double>("init_angle_of_attack");
@@ -64,22 +70,39 @@ namespace lift_drag_force_plugin
 
     // Listen to the update event. This event is broadcast every
     // simulation iteration.
-    this->updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(std::bind(&BuoyancyForcePlugin::OnUpdate, this));
+    this->updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(std::bind(&LiftDragForcePlugin::OnUpdate, this));
   }
 
-  void callbacksub(const std_msgs::msg::Float64MultiArray::SharedPtr msg)
+  void LiftDragForcePlugin::callbacksub(const std_msgs::msg::Float64MultiArray::SharedPtr msg)
   {
-  this->true_wind_speed = msg->data[0];
-  this->true_wind_angle = msg->data[1];
+  this->true_fluid_speed = msg->data[0];
+  this->true_fluid_angle = msg->data[1];
   }
+
+
 
   // Called by the world update start event
-  void BuoyancyForcePlugin::OnUpdate()
+  void LiftDragForcePlugin::OnUpdate()
   {    
-    
-    
+    ignition::math::Pose3d Pose = this->link->WorldPose();
+    ignition::math::Vector3d VelLinearB = this->link->RelativeLinearVel();
+    ignition::math::Vector3d VelAngularB = this->link->RelativeAngularVel();
+
+    double u_b = VelLinearB.X();
+    double v_b = VelLinearB.Y();
+
+    double phi = Pose.Roll();
+    double psi = Pose.Yaw();
+
+    double p_b = VelAngularB.X();
+    double r_b = VelAngularB.Z();
+
+    double v_awu = true_fluid_speed*cos(true_fluid_angle - psi) - u_b + (r_b * center_of_pressure.Y());
+    double v_awv =true_fluid_speed*sin(true_fluid_angle - psi)*cos(phi) - v_b - (r_b * center_of_pressure.X()) + (p_b * center_of_pressure.Z()); 
+
   }
-    
+  
+
   // Register this plugin with the simulator
-  GZ_REGISTER_MODEL_PLUGIN(BuoyancyForcePlugin)
+  GZ_REGISTER_MODEL_PLUGIN(LiftDragForcePlugin)
 }
